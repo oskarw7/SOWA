81b2a5a284270256944536fe7e708d15aa0f492d
Mikolaj Wiszniewski <mikowisz@gmail.com>
2026-04-24 20:06:08 +0200

    feat(mm): integrate with jetson and testing env

--- middle-module/lib/src/Controller.cpp
+++ middle-module/lib/src/Controller.cpp
@@ -11,9 +11,11 @@
 #include "sowa-lib/Controller.hpp"
 #include "sowa-lib/Serial.hpp"
 #include "sowa-lib/helpers.hpp"
+//fov szerob/pixele tla
+//fov / pixele 
 
-constexpr float h_scaling = 180.0/1920.0;
-constexpr float v_scaling = 65.0/1080.0;
+constexpr float h_scaling = (1920.0/8192.0 * 360.0) / 1920.0;
+constexpr float v_scaling = (1080.0/4096.0 * 180.0)/1080.0;
 
 constexpr double kMoveThreshold = 0.2;
 
@@ -69,10 +71,10 @@ void Controller::new_move(int x, int y) {
   float target_steps_x = abs(static_cast<float>(target_vec.x() * h_scaling));
   float target_steps_y = abs(static_cast<float>(target_vec.y() * v_scaling));
 
-  // if (std::sqrt(std::pow(target_steps_x, 2) +
-  //     std::pow(target_steps_y, 2)) < kMoveThreshold) {
-  //   return;
-  // }
+   if (std::sqrt(std::pow(target_steps_x, 2) +
+       std::pow(target_steps_y, 2)) < kMoveThreshold) {
+     return;
+  }
 
   if (!testing_mode) {
     packet_t h_pack {
@@ -97,7 +99,7 @@ void Controller::new_move(int x, int y) {
     serial->send(h_pack);
     serial->send(v_pack);
 
-    cout << "packets sent!" << target_steps_x << target_steps_y << endl;
+    cout << "packets sent!" << (h_dir ? "right" : "left") << " " << target_steps_x << " " << (v_dir ? "up" : "down") << " " << target_steps_y << endl;
   } else {
     string h_command =
       string(h_dir ? "right " : "left ") + lexical_cast<string>(target_steps_x);
--- middle-module/main.cpp
+++ middle-module/main.cpp
@@ -11,18 +11,18 @@ constexpr unsigned int kBaudRate = 115200;
 std::ifstream zupa("/tmp/rura");
 
 int main() {
-  // Controller cc("/dev/ttyACM0", kBaudRate);
+  Controller cc("/dev/ttyGS0", kBaudRate);
   // Controller cc("/tmp/virt2", kBaudRate);
-  Controller cc(true);
+  // Controller cc(true);
   std::cout << "zupa" << std::endl;
 
   int x{0}, y{0};
   int counter =0 ;
   while (true){
   while (zupa >> x >> y) {
-    //zupa >> x >> y;
+    // zupa >> x >> y;
+    // std::cout << x << " " << y << std::endl;
     cc.new_move(x, y);
-    std::cout << "MOve: " <<counter++ << std::endl;
   }
   }
   // if (cc.init_device()) {
