4c2d837d8297f0c339a3b5b1d436cf8847447d5e
Mikołaj Wiszniewski <mikowisz@gmail.com>
2026-05-06 12:45:54 +0200

    fix(Drone): delete String type so it runs

--- Gniazdo/Drone/Drone.py
+++ Gniazdo/Drone/Drone.py
@@ -1,29 +1,30 @@
 import threading
 from time import sleep
-import math
-from datetime import datetime
+
 import cv2
+
 from .GpsHandler import GpsHandler
 
 
-
-
 class Drone:
-    
-    def __init__(self,sim):
-        self.base_image = cv2.imread("./Drone/drone.png",cv2.IMREAD_UNCHANGED)
-        self._position = [0,1600,30]
+    def __init__(self, sim):
+        self.base_image = cv2.imread("./Drone/drone.png", cv2.IMREAD_UNCHANGED)
+        self._position = [0, 1600, 30]
         self.scale = 0.1
         self.image = cv2.resize(self.base_image, None, fx=self.scale, fy=self.scale)
         self.lock = threading.Lock()
         self.current_check_point = 0
         self.sim = sim
-        self.check_points = [(600,2000,3), (-500 % self.sim.Scene.image_width,2000,3)]
+        self.check_points = [
+            (600, 2000, 3),
+            (-500 % self.sim.Scene.image_width, 2000, 3),
+        ]
         self.GpsHandler = GpsHandler()
+
     @property
     def position(self):
         with self.lock:
-            return tuple(self._position) 
+            return tuple(self._position)
 
     @position.setter
     def position(self, value):
@@ -32,21 +33,24 @@ class Drone:
 
     def update_position(self, delta, axis):
         with self.lock:
-            self._position[axis] += delta 
-            if axis == 0 :
-                self._position[axis] %= self.sim.Scene.image_width      
+            self._position[axis] += delta
+            if axis == 0:
+                self._position[axis] %= self.sim.Scene.image_width
 
     def start(self):
         self.thread = threading.Thread(target=self.follow_positions, daemon=True)
         self.thread.start()
 
-
-    def get_current_nema_gps_message(self) -> String:
+    def get_current_nema_gps_message(self):
         return self.GpsHandler.generate_nema_gga(*self._position)
 
     def follow_positions(self):
-        self.position = [self.check_points[0][0], self.check_points[0][1], self.check_points[0][2]]
-        dt = 1 / 24 
+        self.position = [
+            self.check_points[0][0],
+            self.check_points[0][1],
+            self.check_points[0][2],
+        ]
+        dt = 1 / 24
         while True:
             completed_counter = 0
             last_checkpoint = self.check_points[self.current_check_point]
@@ -54,13 +58,23 @@ class Drone:
                 (self.current_check_point + 1) % len(self.check_points)
             ]
             v = [
-                
-                ((( next_checkpoint[0] - last_checkpoint[0] + self.sim.Scene.image_width/2) % self.sim.Scene.image_width) - self.sim.Scene.image_width/2 ) / next_checkpoint[3],
+                (
+                    (
+                        (
+                            next_checkpoint[0]
+                            - last_checkpoint[0]
+                            + self.sim.Scene.image_width / 2
+                        )
+                        % self.sim.Scene.image_width
+                    )
+                    - self.sim.Scene.image_width / 2
+                )
+                / next_checkpoint[3],
                 (next_checkpoint[1] - last_checkpoint[1]) / next_checkpoint[3],
                 (next_checkpoint[2] - last_checkpoint[2]) / next_checkpoint[3],
             ]
             print(v[0])
-            axies = [0,1,2]
+            axies = [0, 1, 2]
             not_at_goal = True
             while axies != []:
                 # print(self.position)
@@ -69,9 +83,11 @@ class Drone:
                         self.update_position(v[axis] * dt, axis)
                     else:
                         axies.remove(axis)
-               
+
                 sleep(dt)
-            self.current_check_point = (self.current_check_point + 1) % len(self.check_points)
+            self.current_check_point = (self.current_check_point + 1) % len(
+                self.check_points
+            )
             # print(f"{self.position=} {v=} {self.check_points[self.current_check_point]}")
 
 
