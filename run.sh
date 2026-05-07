46848b276f92c8a70d9f5dd1456b0bf604a88533
Mikolaj Wiszniewski <mikowisz@gmail.com>
2026-05-07 17:44:48 +0200

    chore(gniadzo): add run.sh to automate tests on jetson

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script works when there is a git worktree folder in root directory of the project.
+# It runs detection and mm in parallel
+
+SESSION="dual-run"
+
+if [ ! -p "/tmp/rura" ]; then
+    rm -f /tmp/rura
+    echo "Robie rure"
+    mkfifo /tmp/rura
+fi
+
+
+tmux has-session -t $SESSION 2>/dev/null
+if [ $? -eq 0 ]; then
+    tmux kill-session -t $SESSION
+fi
+
+tmux new-session -d -s $SESSION "cd ../detection_jetson && just run"
+
+tmux split-window -h -t $SESSION "cd middle-module && make build && sudo make run"
+
+tmux attach-session -t $SESSION
